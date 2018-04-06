d3885994d5f85957df253f18bfac5845d9920b9a
hanmo <dlgksah@gmail.com>
2018-04-06 18:27:54 +0900

    - Add Unit Test Code #1 - update, delete 추가 - 테이블간의 관계가 제대로 연결되는지 확인

--- app/src/androidTest/java/com/hanmo/testforlinkingobject/RealmTransactionTest.kt
+++ app/src/androidTest/java/com/hanmo/testforlinkingobject/RealmTransactionTest.kt
@@ -2,22 +2,28 @@ package com.hanmo.testforlinkingobject
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.util.Log
 import com.hanmo.testforlinkingobject.DataModel.ChildTable
 import com.hanmo.testforlinkingobject.DataModel.ParentTable
 import io.realm.Realm
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 
 /**
  * Created by hanmo on 2018. 4. 6..
  */
 @RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class RealmTransactionTest {
 
     lateinit var realm : Realm
+    private val parentId : Long = 1
+    private val childId : Long = 1
 
     @Before
     fun initDB() {
@@ -26,42 +32,29 @@ class RealmTransactionTest {
     }
 
     @Test
-    fun insertParent() {
-
-        val maxId = realm.where(ParentTable::class.java).max("id")
-        val nextId : Long =
-                when(maxId) {
-                    null -> { 1 }
-                    else -> { maxId.toLong() + 1 }
-                }
+    fun AinsertParent() {
 
         val parent = ParentTable() // realm.createObject(Parent::class.java)
-        parent.id = nextId
+        parent.id = parentId
         parent.parentName = "test_ParentName"
 
         realm.executeTransaction {
             it.copyToRealmOrUpdate(parent)
         }
 
-        val testQuery = realm.where(ParentTable::class.java).equalTo("id", nextId).findFirst()
-        assertNotNull(testQuery)
-        testQuery?.let {
-            assertEquals(nextId, it.id)
+        val testParentQuery = realm.where(ParentTable::class.java).equalTo("id", parentId).findFirst()
+        assertNotNull(testParentQuery)
+        testParentQuery?.let {
+            assertEquals(parentId, it.id)
             assertEquals("test_ParentName", it.parentName)
         }
     }
 
     @Test
-    fun insertChild() {
-        val maxId = realm.where(ChildTable::class.java).max("id")
-        val nextId : Long =
-                when(maxId) {
-                    null -> { 1 }
-                    else -> { maxId.toLong() + 1 }
-                }
+    fun BinsertChild() {
 
         val child = ChildTable() //realm.createObject(ChildTable::class.java)
-        child.id = nextId
+        child.id = childId
         child.childName = "test_ChildName"
         child.parentName = "test_ParentName"
 
@@ -72,19 +65,119 @@ class RealmTransactionTest {
             realm.copyToRealmOrUpdate(child)
         }
 
-        val testQuery = realm.where(ChildTable::class.java).equalTo("id", nextId).findFirst()
-        testQuery?.let {
-            assertEquals(nextId, it.id)
+        //Child Data가 제대로 들어갔는지
+        //들어갔다면 ParentTable과의 연동이 제대로 되고있는지
+        val testChildQuery = realm.where(ChildTable::class.java).equalTo("id", childId).findFirst()
+        assertNotNull(testChildQuery)
+        testChildQuery?.let {
+            assertEquals(childId, it.id)
             assertEquals("test_ChildName", it.childName)
             assertEquals("test_ParentName", it.parentName)
 
             assertNotNull(it.parent)
             it.parent?.forEach {
-                assertEquals(1, it.id)
+                assertEquals(parentId, it.id)
                 assertEquals("test_ParentName", it.parentName)
             }
         }
 
+        //ParentTable에서 Child Data가 제대로 들어갔는지 확인
+        val testParentQuery = realm.where(ParentTable::class.java).equalTo("parentName", "test_ParentName").findAll()
+        assertNotNull(testParentQuery)
+        assertNotEquals(true, testParentQuery.isEmpty())
+
+        testParentQuery.forEach {
+            assertNotNull(it.child)
+            assertNotEquals(true, it.child?.isEmpty())
+            it.child?.forEach {
+                assertEquals(childId, it.id)
+                assertEquals("test_ChildName", it.childName)
+                assertEquals("test_ParentName", it.parentName)
+            }
+        }
+
+    }
+
+    @Test
+    fun CupdateChild() {
+        val child = realm.where(ChildTable::class.java).equalTo("childName", "test_ChildName").findFirst()
+
+        assertNotNull(child)
+
+        realm.executeTransaction {
+            child?.childName = "test_UpdateChildName"
+        }
+
+        val testChildQuery = realm.where(ChildTable::class.java).equalTo("id", childId).findFirst()
+        assertNotNull(testChildQuery)
+        testChildQuery?.let {
+            assertEquals(childId, it.id)
+            assertEquals("test_UpdateChildName", it.childName)
+            assertEquals("test_ParentName", it.parentName)
+
+            assertNotNull(it.parent)
+            it.parent?.forEach {
+                assertEquals(parentId, it.id)
+                assertEquals("test_ParentName", it.parentName)
+            }
+        }
+
+        val testParentQuery = realm.where(ParentTable::class.java).equalTo("parentName", "test_ParentName").findAll()
+        assertNotNull(testParentQuery)
+        assertNotEquals(true, testParentQuery.isEmpty())
+        testParentQuery.forEach {
+            assertNotNull(it.child)
+            assertNotEquals(true, it.child?.isEmpty())
+            it.child?.forEach {
+                assertEquals(childId, it.id)
+                assertEquals("test_UpdateChildName", it.childName)
+                assertEquals("test_ParentName", it.parentName)
+            }
+        }
+    }
+
+    @Test
+    fun DdeleteChild() {
+
+        val testQuery = realm.where(ParentTable::class.java).equalTo("id", parentId).findFirst()
+        Log.e("hanmolee P.child 1", testQuery?.child?.first().toString())
+        Log.e("hanmolee P.child 2", testQuery?.child?.size.toString())
+
+        val childTable = realm.where(ChildTable::class.java).equalTo("id", childId).findFirst()
+        realm.executeTransaction {
+            childTable?.deleteFromRealm()
+        }
+
+        val testQuery2 = realm.where(ParentTable::class.java).equalTo("id", parentId).findFirst()
+        Log.e("hanmolee P.child 3", testQuery2?.child?.size.toString())
+
+        val testParentQuery = realm.where(ParentTable::class.java).equalTo("id", parentId).findFirst()
+        assertNotNull(testParentQuery)
+        testParentQuery?.child?.forEach {
+            Log.e("hanmolee", it.toString())
+        }
+        
+    }
+
+    @Test
+    fun Zdelete() {
+        Log.e("delete", "delete")
+        val parentTable = realm.where(ParentTable::class.java).findAll()
+        val childTable = realm.where(ChildTable::class.java).findAll()
+
+        realm.executeTransaction {
+            parentTable.deleteAllFromRealm()
+            childTable.deleteAllFromRealm()
+        }
+    }
+
+    @After
+    fun closeDB() {
+        assertNotEquals(true, realm.isClosed)
+        if (!realm.isClosed){
+            realm.close()
+            assertEquals(true, realm.isClosed)
+        }
     }
 
 }
